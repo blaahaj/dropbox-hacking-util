43b95b92ec10b3179f7118e8318a45a650077dc7
Rachel Evans <rachel@rachelevans.org>
2026-08-12 20:34:09 +0200

    feat!: strip out the extremely hacky 'auto retry / rate limit' logic

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,4 @@ export * from "./global-options/index.js";
 export * from "./logging.js";
 export * from "./promises/promiseLimiter.js";
 export * from "./promises/simplePromiseRetrier.js";
-export * from "./retry-and-rate-limit/index.js";
 export * from "./time.js";
